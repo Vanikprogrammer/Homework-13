402244b2fdc738575b9c819eb7afe0c4300e76f7
Ivan <ngk-trade@mail.ru>
2017-09-25 23:09:11 +0300

    Не работает почему - то

---
+++ src/main/java/Window.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by 1 on 25.09.2017.
+ */
+public class Window {
+}
---
+++ src/main/java/entitys/ContentDetails.java
@@ -0,0 +1,7 @@
+package entitys;
+
+/**
+ * Created by 1 on 25.09.2017.
+ */
+public class ContentDetails {
+}
